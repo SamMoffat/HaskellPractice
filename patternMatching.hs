190fb8a0ec1c58546e204f08c278cdd5a60e456a
Sam Moffat <sam.moffat@hotmail.com>
2018-05-07 22:55:47 +0100

    finished sytax in functions and started recursion lessons.

--- patternMatching.hs
+++ patternMatching.hs
@@ -38,4 +38,64 @@ bmiTell bmi
   | bmi <= 18.5 = "Underweight"
   | bmi <= 25.0 = "Average weight"
   | bmi <= 30.0 = "Overweight"
-  | otherwise     = "This is not a human's BMI." 
+  | otherwise   = "This is not a human's BMI."
+
+bmiTell' :: (RealFloat a) => a -> a -> String
+bmiTell' height weight 
+  | bmi <= skinny = "Underweight"
+  | bmi <= normal = "Average weight"
+  | bmi <= fat    = "Overweight"
+  | otherwise     = "This is not a human's BMI."
+  where bmi    = height / weight ^ 2
+        skinny = 18.5
+        normal = 25.0
+        fat    = 30.0
+
+max' :: (Ord a) => a -> a -> a
+max' a b | a > b = a | otherwise = b
+
+myCompare :: (Ord a) => a -> a -> Ordering
+a `myCompare` b
+  | a > b     = GT
+  | a == b    = EQ
+  | otherwise = LT
+
+initials :: String -> String -> String
+initials first last = [f] ++ ". " ++ [l] ++ ". "
+  where (f:_) = first
+        (l:_) = last
+
+calcBmis :: (RealFloat a) => [(a, a)] -> [a]
+calcBmis xs = [bmi a b | (a, b) <- xs ]
+  where bmi a b = a / b ^ 2
+
+calcBmis' :: (RealFloat a) => [(a, a)] -> [a]
+calcBmis' xs = [bmi | (h, w) <- xs, let bmi = h / w ^ 2, bmi >=25.0]
+
+cylinder :: (RealFloat a) => a -> a -> a
+cylinder r h = 
+  let sideArea = 2 * pi * r * h
+      topArea = pi * r ^ 2
+  in  sideArea + 2 * topArea
+
+head' :: [a] -> a
+head' [] = error "no items in list!"
+head' (x:_) = x
+
+head'' :: [a] -> a
+head'' xs = case xs of [] -> error "no items in list!"
+                       (x:_) -> x
+
+describeList :: [a] -> String  
+describeList xs = "The list is " ++ case xs of [] -> "empty."  
+                                               [x] -> "a singleton list."   
+                                               xs -> "a longer list."
+
+describeList :: [a] -> String  
+describeList xs = "The list is " ++ what xs  
+  where what [] = "empty."  
+        what [x] = "a singleton list."  
+        what xs = "a longer list."                                                 
+
+--cases and lets can be used anywhere, in the middle of a concat there could be 
+--    "This string is " ++ case of...
